8cecba9b3e201a9875a6e40e644ed34428c0f8c3
William Zapata <55813562+Wize6669@users.noreply.github.com>
2021-09-08 22:11:01 -0500

    Update Usuario
    Update Usuario

--- ProyectoGestionBTS/src/main/java/Source/Usuario.java
+++ ProyectoGestionBTS/src/main/java/Source/Usuario.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Source;
 
-/**
- *
- * @author nicol
- */
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Usuario {
-    
+
+    String user;
+    String contrasena;
+    ArrayList<Integer> nivelesAprobados;
+
+    public Usuario() {
+        nivelesAprobados = new ArrayList<>();
+
+    }
+
+    public void guardarNivel(int nivel, String usuario, String password) {
+        String cadena = "";
+        File archivo;// Manipular archivo
+        FileWriter fw;//Para escribir en el txt
+        PrintWriter pw;
+        archivo = new File("usuarios.txt");
+        if (!archivo.exists()) {
+            try {
+                archivo.createNewFile();
+                usuario = "USERTEST";// usuario = user;
+                cadena += usuario + ", ";
+                password = "123"; // password = contrasena;
+                cadena += password + ", ";
+                nivelesAprobados.add(nivel);
+                fw = new FileWriter(archivo, true);
+                pw = new PrintWriter(fw);
+                Iterator<Integer> nivelesIterator = nivelesAprobados.iterator();
+                while (nivelesIterator.hasNext()) {
+                    cadena += String.valueOf(nivelesIterator.next()) + ", ";
+                }
+                pw.println(cadena + "\n");
+                pw.close();
+                fw.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            try {
+                usuario = "USERTEST";// usuario = user;
+                cadena += usuario + ", ";
+                password = "123"; // password = contrasena;
+                cadena += password + ", ";
+                nivelesAprobados.add(nivel);
+                fw = new FileWriter(archivo, true);
+                pw = new PrintWriter(fw);
+                Iterator<Integer> nivelesIterator = nivelesAprobados.iterator();
+                while (nivelesIterator.hasNext()) {
+                    cadena += String.valueOf(nivelesIterator.next()) + ", ";
+                }
+                pw.println(cadena + "\n");
+                pw.close();
+                fw.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
 }
