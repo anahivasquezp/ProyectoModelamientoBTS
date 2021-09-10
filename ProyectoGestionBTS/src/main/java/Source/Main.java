11d7dc5714fd7c4882b757f0039b50aa4d7955ea
William Zapata <55813562+Wize6669@users.noreply.github.com>
2021-09-09 21:15:30 -0500

    Test II
    Test II

--- ProyectoGestionBTS/src/main/java/Source/Main.java
+++ ProyectoGestionBTS/src/main/java/Source/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         Usuario usuario = new Usuario();
-        usuario.guardarNivel(1, "USER", "123");
+        usuario.guardarNivel(2, "USER", "123");
         System.out.println("Hola");
     }
     
